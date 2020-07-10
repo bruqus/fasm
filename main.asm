98f0f819dfa64b750fc83fcf4a76323d00c87aec
Nikita <bruqusg@gmail.com>
2020-07-10 13:12:12 +0500

    add printf, print oct, print hex functions

--- main.asm
+++ main.asm
@@ -6,11 +6,17 @@ include "asmlib/mth.inc"
 include "asmlib/sys.inc"
 include "asmlib/str.inc"
 
+section '.data' writable
+  fmt db "[%c + %s + %% + %d]", 0
+  msg db "hello", 0
+  msg2 db "World", 0
+
 section '.text' executable
   _start:
-    mov rax, 30
-    mov rbx, 20
-    call gcd 
-    call print_number
+    push 0x16
+    push msg
+    push 'c'
+    mov rax, fmt
+    call printf
     call print_line
     call exit
