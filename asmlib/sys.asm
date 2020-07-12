fa1ddee3f13d805c0f124e6609e1bcabd7ba592f
Nikita <bruqusg@gmail.com>
2020-07-12 23:08:24 +0500

    add input char-string-number, time, srand, rand functions

--- asmlib/sys.asm
+++ asmlib/sys.asm
@@ -1,6 +1,18 @@
 format ELF64
 
 public exit
+public time
+
+section '.time' executable
+  ; | ouput:
+  ; rax = number
+  time:
+    push rbx
+    mov rax, 13
+    xor rbx, rbx
+    int 0x80
+    pop rbx	
+    ret
 
 section '.exit' executable
   exit:
